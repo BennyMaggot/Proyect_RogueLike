3c3058a8eabd90fa50ddfb36576ea8d28527932c
vicsay1 <vicisay@hotmail.com>
2018-12-01 22:44:14 -0600

    avance_domingo
    El player ya se mueve, la camara lo sigue y el player apunta a donde esta el mouse.

---
+++ Proyect_RogueLike/Assets/scrips/playercontrol.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontrol : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+      mirar_al_mouse();
+	}
+
+    void mirar_al_mouse()
+    {
+        var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        var angulo = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angulo,Vector3.down);
+    }
+    
+}
---
+++ Proyect_RogueLike/Assets/scrips/playermove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        mover_al_jugador();
+
+    }
+    void mover_al_jugador()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, 0.1f);
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(0, 0, -0.1f);
+            }
+            else
+            {
+                if (Input.GetKey(KeyCode.A))
+                {
+                    transform.Translate(-0.1f, 0, 0);
+                }
+                else
+                {
+                    if (Input.GetKey(KeyCode.D))
+                    {
+                        transform.Translate(0.1f, 0, 0);
+                    }
+                }
+            }
+        }
+    }
+}
