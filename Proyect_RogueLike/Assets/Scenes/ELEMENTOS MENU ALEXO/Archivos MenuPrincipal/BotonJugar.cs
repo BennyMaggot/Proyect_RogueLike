b6bd9c0d455df70fd9fb0bf0dce4dad26268024a
Benny Santoyo Pérez <maggot15aseeck@gmail.com>
2019-09-30 23:04:50 -0500

    Se creo el menu principal
    Se creo todo el menu principal funcional

---
+++ Proyect_RogueLike/Assets/Scenes/ELEMENTOS MENU ALEXO/Archivos MenuPrincipal/BotonJugar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BotonJugar : MonoBehaviour
+{
+    public Animator Anim;
+    private void OnMouseDown()
+    {
+        StartCoroutine("AnimacionClic");
+    }
+
+    IEnumerator AnimacionClic()
+    {
+        Anim.Play("JugarAnim");
+        yield return new WaitForSeconds(0.2f);
+        SceneManager.LoadScene("Game");
+    }
+}
